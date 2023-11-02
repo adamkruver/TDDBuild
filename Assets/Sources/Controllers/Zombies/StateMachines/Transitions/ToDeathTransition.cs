6ed0da4553b5928ddccaff3f9c06b2dad9e849a0
Klavikus <912654q@gmail.com>
2023-11-02 10:48:03 +0300

    Add base ZombieStateMachine implementation

---
+++ Assets/Sources/Controllers/Zombies/StateMachines/Transitions/ToDeathTransition.cs
@@ -0,0 +1,19 @@
+﻿using Sources.Domain.Zombies;
+using Sources.Infrastructure.FiniteStateMachines.Transitions;
+using Sources.InfrastructureInterfaces.FiniteStateMachines;
+
+namespace Sources.Controllers.Zombies.StateMachines.Transitions
+{
+    public class ToDeathTransition : TransitionBase
+    {
+        private readonly Zombie _zombie;
+
+        public ToDeathTransition(IFiniteState nextState, Zombie zombie) : base(nextState)
+        {
+            _zombie = zombie;
+        }
+
+        protected override bool CanMoveNextState() => 
+            _zombie.Health.Points.Value <= 0;
+    }
+}
