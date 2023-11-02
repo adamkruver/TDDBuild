a3fd57780c0bdb525c7b9a8d7e7c1b41a4db03d5
Алексей Кононов <kruver@mail.ru>
2023-11-03 02:45:31 +0300

    Changed WeaponFabs

---
+++ Assets/Sources/Infrastructure/Factories/Presentation/Views/WallConstructionViewFactory.cs
@@ -0,0 +1,13 @@
+﻿using Sources.Domain.Walls;
+using Sources.PresentationInterfaces.Views.Constructions;
+
+namespace Sources.Infrastructure.Factories.Presentation.Views
+{
+    public class WallConstructionViewFactory
+    {
+        public IConstructionView Create(Wall wall)
+        {
+            return null;
+        }
+    }
+}
