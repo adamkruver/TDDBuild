ae8ad546854d4145992cc6e2fd5f135fc89c071e
Алексей Кононов <kruver@mail.ru>
2023-11-04 17:21:57 +0300

    Added Weapon Factories

---
+++ Assets/Sources/Infrastructure/Factories/Domain/Weapons/SingleGunFactory.cs
@@ -0,0 +1,26 @@
+﻿using Sources.Domain.Systems.Upgrades;
+using Sources.Domain.Weapons;
+using Sources.Infrastructure.Factories.Domain.Bullets;
+using Sources.InfrastructureInterfaces.Services.Times;
+
+namespace Sources.Infrastructure.Factories.Domain.Weapons
+{
+    public class SingleGunFactory : WeaponFactoryBase<SingleGun>
+    {
+        private readonly BulletFactory _bulletFactory;
+        private readonly UpgradeSystem _upgradeSystem;
+
+        public SingleGunFactory(
+            BulletFactory bulletFactory,
+            ITimeService timeService,
+            UpgradeSystem upgradeSystem
+        ) : base(timeService)
+        {
+            _bulletFactory = bulletFactory;
+            _upgradeSystem = upgradeSystem;
+        }
+
+        public override IWeapon Create() =>
+            new SingleGun(_bulletFactory.Create(), TimeService, WeaponFab, _upgradeSystem);
+    }
+}
