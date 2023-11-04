0976678b531c49fd871e83b2eeda37f9f49ae626
Алексей Кононов <kruver@mail.ru>
2023-11-04 17:25:55 +0300

    Added Weapon StateMachineFactories

---
+++ Assets/Sources/Infrastructure/Factories/Controllers/Weapons/TripleGunStateMachineFactory.cs
@@ -0,0 +1,42 @@
+﻿using Sources.Controllers.Weapons.StateMachines.Lasers.States;
+using Sources.Controllers.Weapons.StateMachines.Lasers.Transitions;
+using Sources.Domain.Weapons;
+using Sources.InfrastructureInterfaces.FiniteStateMachines;
+using Sources.InfrastructureInterfaces.Services.Weapons;
+using Sources.Presentation.Views.Weapons;
+using Sources.PresentationInterfaces.Views.Systems.TargetTrackers;
+using Sources.PresentationInterfaces.Views.Weapons;
+
+namespace Sources.Infrastructure.Factories.Controllers.Weapons
+{
+    public class TripleGunStateMachineFactory : WeaponStateMachineFactoryBase
+    {
+        protected override IFiniteState CreateStates(
+            ICompositeWeaponView compositeView,
+            IWeaponView[] views,
+            IWeapon weapon,
+            ITargetTrackerSystem targetTrackerSystem,
+            IWeaponService service
+        )
+        {
+            TrackTargetState trackTargetState = new TrackTargetState(weapon, targetTrackerSystem, service);
+            ShootState shootState = new ShootState(views, weapon);
+
+            ToShootStateTransition toShootStateTransition = new ToShootStateTransition(
+                shootState, weapon, targetTrackerSystem, service
+            );
+
+            CooldownState cooldownState = new CooldownState();
+
+            ToCooldownTransition toCooldownTransition = new ToCooldownTransition(cooldownState, weapon);
+
+            ToTrackTargetTransition toTrackTargetTransition = new ToTrackTargetTransition(trackTargetState, weapon);
+
+            trackTargetState.AddTransition(toShootStateTransition);
+            shootState.AddTransition(toCooldownTransition);
+            cooldownState.AddTransition(toTrackTargetTransition);
+
+            return trackTargetState;
+        }
+    }
+}
