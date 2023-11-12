9efa6a00228268ea2bdd2666b83558a1964082a8
Klavikus <912654q@gmail.com>
2023-11-12 08:30:43 +0300

    Remove redundant spaces

--- Assets/Sources/Infrastructure/Repositories/EnemyRepository.cs
+++ Assets/Sources/Infrastructure/Repositories/EnemyRepository.cs
@@ -6,14 +6,14 @@ namespace Sources.Infrastructure.Repositories
     public class EnemyRepository
     {
         private readonly List<IEnemy> _enemies = new List<IEnemy>();
-        
-        public void Add(IEnemy enemy) => 
+
+        public void Add(IEnemy enemy) =>
             _enemies.Add(enemy);
 
-        public void Remove(IEnemy enemy) => 
+        public void Remove(IEnemy enemy) =>
             _enemies.Remove(enemy);
 
-        public IReadOnlyCollection<IEnemy> GetAll() => 
+        public IReadOnlyCollection<IEnemy> GetAll() =>
             _enemies.ToArray();
     }
 }
